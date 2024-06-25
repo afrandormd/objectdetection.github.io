e2dd7c8f3d939e355396dc2c3c42cdfdf292f646
Afrando Sharein Ramadhan <afrandormd@gmail.com>
2024-06-25 20:31:37 +0700

    commit pertama

---
+++ precache-manifest.a2fb2b14b5edfcb69a929eba9012149a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd9dc863760c0850f60a987be15cf90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7714348c994a7eb19e53",
+    "url": "/static/css/main.aebc1bcc.chunk.css"
+  },
+  {
+    "revision": "0f55b589a6abd05815d5",
+    "url": "/static/js/2.74d5ba8c.chunk.js"
+  },
+  {
+    "revision": "efee2495fc7aace2096c537dd5098d79",
+    "url": "/static/js/2.74d5ba8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7714348c994a7eb19e53",
+    "url": "/static/js/main.b694f422.chunk.js"
+  },
+  {
+    "revision": "fd4a854c06bb36be3a7d",
+    "url": "/static/js/runtime-main.1530841d.js"
+  }
+]);
